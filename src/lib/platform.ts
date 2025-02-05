f008dc893f4b78ee8989f91b358a0b685239de17
0xnewton <0xnewton@protonmail.com>
2025-02-05 14:40:46 -0500

    adds firebase config

---
+++ src/lib/platform.ts
@@ -0,0 +1,9 @@
+import * as admin from "firebase-admin";
+import { getFunctions } from "firebase-admin/functions";
+
+const app = admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+export const db = admin.firestore(app);
+export const functions = getFunctions(app);
